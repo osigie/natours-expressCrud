721f3e92fec4e05cf219a4591a2860170101acae
usser.email <user@MacBook-Air-de-user.local>
2022-04-02 11:02:05 +0100

    fixed static page

--- app.js
+++ app.js
@@ -9,29 +9,13 @@ const tourRoute = require('./routes/tourRoutes')
 const userRoute = require('./routes/userRoutes')
 ///middleware
 app.use(express.json());
-
+app.use(express.static(`${__dirname}/public`))
 app.use(morgan('combined'));
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-
-
-
-
-
-// ///GET all tours
-// app.get('/api/v1/tours', getAllTours);
-
-// ///POST
-// app.post('/api/v1/tours', createTours);
-// //GET single tour
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
-//Routes
 
 
 
